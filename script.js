a54f5bc82eb4d9b24f2ad48c159a10098d9e868c
Thomas Steimann <thomas@steimann.ch>
2025-03-10 23:44:47 +0100

    url Animation added

--- script.js
+++ script.js
@@ -1,23 +1,138 @@
 let prism = document.querySelector(".rec-prism");
 
 function showSignup() {
-    prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
+  prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
 }
 function showLogin() {
-    prism.style.transform = "translateZ(-100px)";
+  prism.style.transform = "translateZ(-100px)";
 }
 function showForgotPassword() {
-    prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
+  prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
 }
 
 function showSubscribe() {
-    prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
+  prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
 }
 
 function showContactUs() {
-    prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
+  prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
 }
 
 function showThankYou() {
-    prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
+  prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
 }
+
+let myEmojis = [
+  "😀",
+  "😂",
+  "😇",
+  "😍",
+  "🤩",
+  "🥳",
+  "🥺",
+  "😎",
+  "🤓",
+  "🥶",
+  "🥵",
+  "😱",
+  "😭",
+  "😡",
+  "🥴",
+  "🤢",
+  "🤮",
+  "🤠",
+  "🤡",
+  "🥸",
+  "🤥",
+  "🤫",
+  "🤭",
+  "🧐",
+  "🤔",
+  "🤯",
+  "🥱",
+  "🤤",
+  "🤑",
+  "🤗",
+  "🤭",
+  "🤫",
+  "🤔",
+  "🤐",
+  "🤨",
+  "🤥",
+  "🤢",
+  "🤮",
+  "🤧",
+  "🥵",
+  "🥶",
+  "🥴",
+  "😵",
+  "🤯",
+  "🤠",
+  "🥳",
+  "🥺",
+  "🤩",
+  "😇",
+  "😍",
+  "🤪",
+  "😜",
+  "😝",
+  "🤑",
+  "🤗",
+  "🤭",
+  "🤫",
+  "🤔",
+  "🤐",
+  "🤨",
+  "🤥",
+  "🤢",
+  "🤮",
+  "🤧",
+  "🥵",
+  "🥶",
+  "🥴",
+  "😵",
+  "🤯",
+  "🤠",
+  "🥳",
+  "🥺",
+  "🤩",
+  "😇",
+  "😍",
+  "🤪",
+  "😜",
+  "😝",
+  "🤑",
+  "🤗",
+  "🤭",
+  "🤫",
+  "🤔",
+  "🤐",
+  "🤨",
+  "🤥",
+  "🤢",
+  "🤮",
+  "🤧",
+  "🥵",
+  "🥶",
+  "🥴",
+  "😵",
+  "🤯",
+  "🤠",
+  "🥳",
+  "🥺",
+  "🤩",
+  "😇",
+  "😍",
+  "🤪",
+  "😜",
+  "😝",
+  "🤑",
+];
+
+let urlAnimate = () => {
+  window.location.hash =
+    myEmojis[Math.floor((Date.now() / 100) % myEmojis.length)];
+  setTimeout(urlAnimate, 70);
+};
+
+urlAnimate();
